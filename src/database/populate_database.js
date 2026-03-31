e5edf3e92c205073b490844ccfd45ecc3edda613
NepSauce <greasyandmoist@gmail.com>
2026-03-31 18:16:34 -0300

    Chore: Rename database table column

--- src/database/populate_database.js
+++ src/database/populate_database.js
@@ -2,7 +2,7 @@ const db = require('./database.js');
 
 const createTableQuery = `
 CREATE TABLE IF NOT EXISTS repositories (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    repository_id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     contributors_count INTEGER NOT NULL,
     stars_count INTEGER NOT NULL
